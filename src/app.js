9ed35f6bb341175b7c3d6378cc9a3bd491f3c510
孙昊翔 <673686754@qq.com>
2018-01-16 21:12:48 +0800

    no message

--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
+import {
+    BrowserRouter as Router,
+    Route
+} from 'react-router-dom';
+
+import Frame from 'frame/Frame';
+
+require('semantic/dist/semantic.min.css');
+require('semantic/dist/semantic.min.js');
 
 ReactDOM.render(
-    <div>Hello</div>,
+    <Router>
+        <Route path="/" component={Frame}/>
+    </Router>,
     document.getElementById('root')
 );
 
