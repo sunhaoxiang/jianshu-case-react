d8df41c1193c08372c738204c3b9c4fd06aff2cf
孙昊翔 <673686754@qq.com>
2018-01-17 08:03:21 +0800

    no message

--- src/layout/frame/Frame.js
+++ src/layout/frame/Frame.js
@@ -3,7 +3,7 @@ import Nav from 'nav/Nav';
 import Home from 'view/home/Home.js';
 import S from './style.scss';
 
-export default class Layout extends React.Component{
+export default class Frame extends React.Component{
     constructor(props){
         super(props);
     }
